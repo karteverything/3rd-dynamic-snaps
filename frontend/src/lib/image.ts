4d60a24331598b259f2a8dfdf397f2f804aca8a9
Mlungisi <sibitanemlungisi26@gmail.com>
2026-09-22 11:58:50 +0200

    fix file naming

--- frontend/src/lib/image.ts
+++
@@ -1,7 +0,0 @@
-const API_URL = import.meta.env.VITE_API_URL;
-
-export function getPhotoUrl(storagePath: string) {
-  return `${API_URL}/api/photos/${encodeURIComponent(
-    storagePath,
-  )}/url`;
-}
